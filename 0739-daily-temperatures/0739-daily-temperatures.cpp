74875efacef8de45527a734b2c61174b33e63ff3
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-01 09:11:58 +0545

    Time: 28 ms (29.16%), Space: 107.4 MB (18.97%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        int n = temperatures.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            while (st.size() > 0 && temperatures[i] > temperatures[st.top()]) {
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
