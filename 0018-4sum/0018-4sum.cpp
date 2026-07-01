1f604229bb5ba6207eaa9e0b063b2a4524aee193
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-01 17:21:39 +0545

    Time: 23 ms (54.54%), Space: 17.2 MB (99%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n =nums.size();
-        set<vector<int>>v;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                set<long long> st;
-                for(int k=j+1;k<n;k++){
-                    long long x = (long long)target - nums[i] - nums[j] - nums[k];
-                    if(st.find(x)!=st.end()){
-                        vector<int>temp={nums[i],nums[j],nums[k],(int)x};
-                        sort(temp.begin(),temp.end());
-                        v.insert(temp);
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i - 1] == nums[i])continue;
+            for (int j = i + 1; j < n; j++) {
+                int k = j + 1;
+                int l = n - 1;
+                 if (j > i + 1 && nums[j - 1] == nums[j])continue;
+                while (k < l) {
+                   long long x = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if (x > target) {
+                        l--;
+                    } else if (x < target) {
+                        k++;
+                    } else {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        l--;
+                        k++;
+                        while (k<l && nums[l] == nums[l + 1])l--;
+                        while (k<l && nums[k] == nums[k - 1]) k++;
                     }
-                    st.insert(nums[k]);
                 }
             }
         }
-        vector<vector<int>>ans(v.begin(),v.end());
         return ans;
     }
 };
