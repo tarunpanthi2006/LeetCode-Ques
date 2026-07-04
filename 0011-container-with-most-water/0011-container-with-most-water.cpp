f7fd314cdffd5bc319925082765aa38de2507e72
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-04 22:14:31 +0545

    Time: 4 ms (20.58%), Space: 63 MB (10.72%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& arr) {
+        int n=arr.size();
+        int left=0;
+        int right=n-1;
+        int maxarea=0;
+        while(left<right){
+            int limiting=min(arr[left],arr[right]);
+            int area=(right-left)*limiting;
+            if(limiting==arr[left])left++;
+            else right--;
+            maxarea=max(area,maxarea);
+        }
+        return maxarea;
+    }
+};
