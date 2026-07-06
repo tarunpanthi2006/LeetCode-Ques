a43fe749a77c2251a0325216751b7ef28cae0274
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-06 22:55:30 +0545

    Time: 12 ms (12.85%), Space: 15 MB (82.12%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -6,9 +6,8 @@ public:
         int ans=n;
         for(int i=n-1;i>=0;i--){
             for(int j=n-2;j>=0;j--){
-                int k=0;
                 if(i==j)continue;
-                if(nums[i][k]>=nums[j][k] && nums[i][k+1]<=nums[j][k+1]){
+                if(nums[i][0]>=nums[j][0] && nums[i][1]<=nums[j][1]){
                     ans--;
                     break;
                 }
