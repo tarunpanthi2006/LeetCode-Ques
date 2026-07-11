ded6a5a92961e9ba59a57ce35d46bf8edf5ede3f
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-11 23:29:29 +0545

    Time: 93 ms (5.12%), Space: 44.6 MB (13.01%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int n=s.length();
+        string ans="";
+        int count=1;
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                if(s[i]==s[i-1]){
+                    count++;
+                }
+                else count=1;
+            }
+            ans+=s[i];
+            if(count>=3){
+                ans.pop_back();
+            }
+        }
+        return ans;
+    }
+};
