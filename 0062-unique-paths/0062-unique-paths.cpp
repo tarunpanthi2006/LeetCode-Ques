5040a5cee6aa0377a93a2cba04f929abed22660e
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-12 22:09:43 +0545

    Time: 0 ms (100%), Space: 9.4 MB (30.28%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int helper(int sr,int sc,int er,int ec,vector<vector<int>>&dp){
+        if(sr==er && sc==ec)return 1;
+        if(sr>er || sc>ec)return 0;
+        if(dp[sr][sc]!=-1)return dp[sr][sc];
+        dp[sr][sc]=helper(sr,sc+1,er,ec,dp)+helper(sr+1,sc,er,ec,dp);
+        return dp[sr][sc];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return helper(0,0,m-1,n-1,dp);
+    }
+};
