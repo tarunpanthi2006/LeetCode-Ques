bdcb425cfd27ea524f153eff71ae10a46ebf504c
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-14 16:37:18 +0545

    Time: 3 ms (23.48%), Space: 10.5 MB (79.5%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
+        vector<int>ans(n+1);
         for(int i=0;i<=n;i++){
-            int x = i;
-            int c=0;
-            while(x>0){
-                if(x%2==1)c++;
-                x=x/2;
+            int count=0;
+            int x=i;
+            while(x!=0){
+                if((x&1)==1)count++;
+                x>>=1;  // >> is the right shift operator
             }
-            ans.push_back(c);
+            ans[i]=count;
         }
         return ans;
     }
