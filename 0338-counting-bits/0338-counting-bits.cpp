37e88332c4d698f3376cc67e0240c45c8c8f20e0
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-14 17:00:44 +0545

    Time: 0 ms (100%), Space: 10.6 MB (56.82%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+// sab commnets achhe se padhna samajne k liye
     vector<int> countBits(int n) {
-        vector<int>ans(n+1);
-        for(int i=0;i<=n;i++){
-            int count=0;
-            int x=i;
-            while(x!=0){
-                if((x&1)==1)count++;
-                x>>=1;  // >> is the right shift operator
-            }
-            ans[i]=count;
+        vector<int> dp(n + 1, 0);
+        // basic se observation ki odd number ki last bit is always 1. and even number ka last bit is always 0.
+
+        // toh simple sa formula bana diya ki agar odd hai toh pichle wale se ek jyada hoga and if even toh add nahi hoga 
+        for (int i = 1; i <= n; i++) {
+            // Simple division and modulo approach
+
+            // issme ">>"isska kaam 1 bit aage badna hai. mai i>>1 ki jagha (i/2) bhi use kar sakta hu vo bhi same kaam karta hai
+
+            // and i%2 check kar raha hai ki even hai ya odd
+
+            // mai i%2 ki jagha (i&1)bhi likh sakta hu coz vo bhi yahi check karta hai
+            dp[i] = dp[i>> 1] + (i % 2);
         }
-        return ans;
+        
+        return dp;
     }
 };
