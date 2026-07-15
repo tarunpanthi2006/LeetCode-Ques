5528b830654e8c46646e48eb421c96e36c07de7b
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-15 20:03:26 +0545

    Time: 0 ms (100%), Space: 10.8 MB (23.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int recursion(vector<int>&nums,int i,vector<int>&dp){
-        int n=nums.size();
-        if(i>=n)return 0;
-        if(dp[i]!=-1)return dp[i];
-        dp[i]=max(0+recursion(nums,i+1,dp),nums[i]+recursion(nums,i+2,dp));
+    int recursion(vector<int>& nums, int i, vector<int>& dp) {
+        int n = nums.size();
+        if (i == n - 1)return nums[n - 1];
+        if (i == n - 2)return max(nums[n - 1], nums[n - 2]);
+        if (dp[i] != -1)return dp[i];
+        dp[i] = max(0 + recursion(nums, i + 1, dp),nums[i] + recursion(nums, i + 2, dp));
         return dp[i];
-        // basically i have 2 choices leave or steal so dono cases ko tackle kiya
-        // if i am stealing i will add that value to ans and then i=i+2 coz adjacent wale ko ignore karunga
-        // if i am leaving then i will go to i+1;
+        // basically i have 2 choices leave or steal so dono cases ko tackle
+        // kiya if i am stealing i will add that value to ans and then i=i+2 coz
+        // adjacent wale ko ignore karunga if i am leaving then i will go to
+        // i+1;
     }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return recursion(nums,0,dp);
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return recursion(nums, 0, dp);
     }
 };
