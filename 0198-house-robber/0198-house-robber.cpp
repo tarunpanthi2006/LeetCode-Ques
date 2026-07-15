6cbda19c0fbb82c5c9b0c68b5d40b1fa4a89e0b5
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-15 19:58:20 +0545

    Time: 0 ms (100%), Space: 10.8 MB (23.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int num1=0;
-        int num2=0;
+    int recursion(vector<int>&nums,int i,vector<int>&dp){
         int n=nums.size();
-        int ans=0;
-        int temp=0;
-        for(int i=0;i<n;i++){
-            ans=max(num1,num2+nums[i]);
-            temp=num1;
-            num1=ans;
-            num2=temp;
-        }
-        return ans;
+        if(i>=n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        dp[i]=max(0+recursion(nums,i+1,dp),nums[i]+recursion(nums,i+2,dp));
+        return dp[i];
+        // basically i have 2 choices leave or steal so dono cases ko tackle kiya
+        // if i am stealing i will add that value to ans and then i=i+2 coz adjacent wale ko ignore karunga
+        // if i am leaving then i will go to i+1;
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return recursion(nums,0,dp);
     }
 };
