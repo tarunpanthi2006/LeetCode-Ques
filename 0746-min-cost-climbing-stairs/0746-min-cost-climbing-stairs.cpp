58775e762eb155e7ca161617643f04396a511572
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-17 12:32:24 +0545

    Time: 0 ms (100%), Space: 17.4 MB (97.31%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int helper(vector<int>cost,int i , vector<int>&dp){
-        int n=cost.size();
-        if(i>=n)return 0; // matlab mai last mein pahuch gaya and ab koi step nahi hai so ab koi new cost nahi aayega so return 0 kara
-        if(dp[i]!=-1)return dp[i];
-        dp[i]=cost[i]+min(helper(cost,i+1,dp),helper(cost,i+2,dp));
-        return dp[i];
-    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int>dp(n,-1);
-        return min(helper(cost,0,dp),helper(cost,1,dp));
+        int prev2=cost[0];
+        int prev1=cost[1];
+        for(int i=2;i<n;i++){
+            int curr=cost[i]+min(prev1,prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return min(prev1,prev2);
     }
 };
