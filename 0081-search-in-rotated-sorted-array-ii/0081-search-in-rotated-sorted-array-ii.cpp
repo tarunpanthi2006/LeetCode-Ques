7a72fa655c1eba921669eb465088d83604079fdc
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-17 22:04:50 +0545

    Time: 0 ms (100%), Space: 18 MB (45.54%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,29 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)return true;
+        int lo=0;
+        int hi=nums.size()-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target)return true;
+            // Handle the duplicate edge case
+            if(nums[lo] == nums[mid] && nums[mid] == nums[hi]) {
+                lo++;
+                hi--;
+                continue; // Skip the rest of the loop and recalculate mid
+            }
+            if(nums[lo]<=nums[mid]){
+                if(target>=nums[lo] && target<nums[mid]){
+                    hi=mid-1;
+                }
+                else lo=mid+1;
+            }
+            else{
+                if(target>nums[mid] && target<=nums[hi]){
+                    lo=mid+1;
+                }
+                else hi=mid-1;
+            }
         }
         return false;
     }
