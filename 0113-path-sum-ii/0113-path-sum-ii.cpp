574faf1d905084d56d3e129a90fa42b6d86e58f3
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-19 18:07:38 +0545

    Time: 0 ms (100%), Space: 20.8 MB (99.52%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> helper(TreeNode* root, int target,vector<vector<int>>&ans,vector<int>&v,int sum){
-        if(root==NULL)return ans;
+    void helper(TreeNode* root, int target,vector<vector<int>>&ans,vector<int>&v,int sum){
+        if(root==NULL)return ;
         v.push_back(root->val);
         sum+=root->val;
         if(root->left==NULL && root->right==NULL){
@@ -12,11 +12,11 @@ public:
         helper(root->left,target,ans,v,sum);
         helper(root->right,target,ans,v,sum);
         v.pop_back();
-        return ans;
     }
     vector<vector<int>> pathSum(TreeNode* root, int target) {
         vector<vector<int>> ans;
         vector<int>v;
-        return helper(root,target,ans,v,0);
+        helper(root,target,ans,v,0);
+        return ans;
     }
 };
