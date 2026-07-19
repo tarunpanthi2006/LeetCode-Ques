3094a5b89ee03cc27f6abf4abc97a5d1f8396202
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-19 10:58:29 +0545

    Time: 0 ms (100%), Space: 22 MB (16.32%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> traversal(TreeNode*root){
+        TreeNode*curr=root;
+        TreeNode* pred=NULL;
+        vector<int>ans;
+        while(curr){
+            if(curr->left!=NULL){
+                pred=curr->left;
+                while(pred->right!=NULL && pred->right!=curr){
+                    pred=pred->right;
+                }
+                if(pred->right==NULL){
+                    pred->right=curr;
+                    curr=curr->left;
+                }
+                if(pred->right==curr){
+                    pred->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+            else{
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
+        return ans;
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int>inorder= traversal(root);
+        int n=inorder.size();
+        for(int i=0;i<n-1;i++){
+            if(inorder[i]>=inorder[i+1])return false;
+        }
+        return true;
+    }
+};
