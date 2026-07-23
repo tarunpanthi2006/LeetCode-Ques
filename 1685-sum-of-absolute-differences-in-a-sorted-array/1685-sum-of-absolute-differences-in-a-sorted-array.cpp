c76e56d258ddde2add6dd9abdfc9c41e0a6610a0
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-23 08:44:50 +0545

    Time: 90 ms (9.03%), Space: 96.1 MB (7.3%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         // prefix sum and suffix sum toh chahiye then nums[i]se aage ke liye sum-kitne aage*nums[i] and nums[i] ke piche k liye kitne piche*nums[i]-sum;
         int n= nums.size();
-        vector<int>prefix;
+        vector<int>prefix(n);
         vector<int>suffix(n);
         int sum=0;
-        for(int i=0;i<n;i++){
-            prefix.push_back(sum);
-            sum+=nums[i];
-        }
         int sum2=0;
-        for(int i=n-1;i>=0;i--){
-            suffix[i]=sum2;
-            sum2+=nums[i];
+        int j=n-1;
+        for(int i=0;i<n;i++){
+            prefix[i]=sum;
+            suffix[j]=sum2;
+            sum+=nums[i];
+            sum2+=nums[j];
+            j--;
         }
         vector<int>ans;
         for(int i=0;i<n;i++){
