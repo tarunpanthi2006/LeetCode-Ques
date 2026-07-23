4ddf751e11ea3d6977f8d4f1a24c738145bf2d48
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-23 10:27:50 +0545

    Time: 0 ms (100%), Space: 29.4 MB (99.13%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool search(vector<int>nums,int n){
+    bool search(vector<int>&nums,int n){
         for(int i=0;i<nums.size();i++){
             if(nums[i]==n)return true;
         }
@@ -10,26 +10,26 @@ public:
         vector<int>ans;
         int n1=nums1.size();
         for(int i=0;i<n1;i++){
+            if(search(ans, nums1[i]) == true) {
+                continue; 
+            }
             bool flag1=search(nums2,nums1[i]);
             if(flag1){
-                if(search(ans,nums1[i])==false) {
-                    ans.push_back(nums1[i]);
-                }
+                ans.push_back(nums1[i]);
                 continue;
             }
             bool flag2=search(nums3,nums1[i]);
             if(flag2){
-                if(search(ans,nums1[i])==false) {
-                    ans.push_back(nums1[i]);
-                }
+                ans.push_back(nums1[i]);
             }
         }
         for(int i=0;i<nums2.size();i++){
+            if(search(ans, nums2[i]) == true) {
+                continue; 
+            }
             bool flag=search(nums3,nums2[i]);
             if(flag){
-                if(search(ans,nums2[i])==false){
-                    ans.push_back(nums2[i]);
-                }
+                ans.push_back(nums2[i]);
             }
         }
         return ans;
