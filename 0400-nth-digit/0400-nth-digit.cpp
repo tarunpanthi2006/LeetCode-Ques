cd704a1bca07227878e22f66513569335c58cc13
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-28 09:29:03 +0545

    Time: 0 ms (100%), Space: 7.8 MB (94.3%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.cpp
+++ 0400-nth-digit/0400-nth-digit.cpp
@@ -1,69 +1,33 @@
 class Solution {
 public:
     int findNthDigit(int n) {
-        if(n<=9)return n;
-        int noofdigit;
-        int t=n;
-        int u;
-        if(n<=180+9){
-            noofdigit=2;
-            t=t-9;
-            u=9;
-        }
-        else if(n<=2700+180+9){
-            noofdigit=3;
-            t=t-9-180;
-            u=99;
-        }
-        else if(n<=36000+2700+180+9){
-            noofdigit=4;
-            t=t-9-180-2700;
-            u=999;
-        }
-        else if(n<=450000+36000+2700+180+9){
-            noofdigit=5;
-            t=t-9-180-2700-36000;
-            u=9999;
-        }
-        else if(n<=5400000+450000+36000+2700+180+9){
-            noofdigit=6;
-            t=t-9-180-2700-36000-450000;
-            u=99999;
-        }
-        else if(n<=63000000+5400000+450000+36000+2700+180+9){
-            noofdigit=7;
-            t=t-9-180-2700-36000-450000-5400000;
-            u=999999;
-        }
-        else if(n<=720000000+63000000+5400000+450000+36000+2700+180+9){
-            noofdigit=8;
-            t=t-9-180-2700-36000-450000-5400000-63000000;
-            u=9999999;
-        }
-        else{
-            noofdigit=9;
-            t=t-9-180-2700-36000-450000-5400000-63000000-720000000;
-            u=99999999;
-        }
-        int x=t%noofdigit;
-        int num;
-        if(x==0){
-            num=u+(t/noofdigit);
-            return num%10;
-        }
-        else{
-            num=u+(t/noofdigit)+1;
-            int q=num;
-            int digit=0;
-            while(q>0){
-                digit++;
-                q=q/10;
+        if (n <= 9)
+            return n;
+        long long temp = n;
+        int range = 0;
+        long long no = 0;
+        long long p10 = 1;
+        for (int i = 0; i <= 9; i++) {
+            no = 9LL * p10 * (i + 1);
+            if (temp <= no) { 
+                range = i + 1;
+                break;
             }
-            int y=pow(10,digit-x);
-            int ans= num/y;
-            int r=ans/10;
-            r=r*10;
-            return ans-r;
+            temp -= no;
+            p10 *= 10;
         }
+        long long pos = temp - 1; 
+        long long num = pos / range + p10;
+        int dig = pos % range;
+        int ans = 0;
+        int cnt = 0;
+        while (num != 0) {
+            ans = num % 10;
+            if (cnt == range - dig - 1)
+                break;
+            cnt++;
+            num /= 10;
+        }
+        return ans;
     }
 };
