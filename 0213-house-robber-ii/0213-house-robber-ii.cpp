703f1787387125e4caa4162fc5fc39c0b290c0e5
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-31 00:44:45 +0545

    Time: 0 ms (100%), Space: 10.9 MB (19.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int helper2(int n,vector<int>& nums,vector<int>&dp2){
-        if (n < 1) return 0;
-        if(n==1)return nums[1];
-        if(dp2[n]!=-1)return dp2[n];
-        int pick=nums[n]+helper2(n-2,nums,dp2);
-        int notpick=0+helper2(n-1,nums,dp2);
-        return dp2[n]=max(pick,notpick);
-    }
-    int helper(int n,vector<int>& nums,vector<int>&dp){
+    int helper(int n,vector<int>&v,vector<int>&dp){
         if (n < 0) return 0;
-        if(n==0)return nums[0];
+        if(n==0)return v[0];
         if(dp[n]!=-1)return dp[n];
-        int pick=nums[n]+helper(n-2,nums,dp);
-        int notpick=0+helper(n-1,nums,dp);
+        int pick=v[n]+helper(n-2,v,dp);
+        int notpick=0+helper(n-1,v,dp);
         return dp[n]=max(pick,notpick);
     }
     int rob(vector<int>& nums) {
@@ -22,8 +14,14 @@ public:
         if(n==1)return nums[0];
         vector<int>dp(n+1,-1);
         vector<int>dp2(n+1,-1);
-        int ans1= helper(n-2,nums,dp);
-        int ans2=helper2(n-1,nums,dp2);
+        vector<int>temp1;
+        vector<int>temp2;
+        for(int i=0;i<n;i++){
+            if(i!=0)temp1.push_back(nums[i]);
+            if(i!=n-1)temp2.push_back(nums[i]);
+        }
+        int ans1= helper(n-2,temp1,dp);
+        int ans2=helper(n-2,temp2,dp2);
         return max(ans1,ans2);
     }
 };
