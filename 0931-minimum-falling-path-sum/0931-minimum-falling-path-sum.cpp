abc336cdd7ac0bdd1e4de0151d564f398610ae4b
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-31 17:57:22 +0545

    Time: 3 ms (38.55%), Space: 16.3 MB (8.05%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int f(int row, int col, vector<vector<int>>& matrix,
-          vector<vector<int>>& dp, vector<vector<bool>>& vis) {
+          vector<vector<int>>& dp) {
 
         int n = matrix.size();
 
@@ -13,12 +13,9 @@ public:
 
         if (dp[row][col]!=INT_MIN)
             return dp[row][col];
-
-       
-
-        int left = f(row - 1, col - 1, matrix, dp, vis);
-        int up = f(row - 1, col, matrix, dp, vis);
-        int right = f(row - 1, col + 1, matrix, dp, vis);
+        int left = f(row - 1, col - 1, matrix, dp);
+        int up = f(row - 1, col, matrix, dp);
+        int right = f(row - 1, col + 1, matrix, dp);
 
         int best = min({left, up, right});
 
@@ -32,12 +29,10 @@ public:
         int n = matrix.size();
 
         vector<vector<int>> dp(n, vector<int>(n,INT_MIN));
-        vector<vector<bool>> vis(n, vector<bool>(n, false));
-
         int ans = INT_MAX;
 
         for (int j = 0; j < n; j++) {
-            ans = min(ans, f(n - 1, j, matrix, dp, vis));
+            ans = min(ans, f(n - 1, j, matrix, dp));
         }
 
         return ans;
