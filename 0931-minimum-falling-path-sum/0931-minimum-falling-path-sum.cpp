7fd6ec94c469a3093ca14228e0e8207668090a5e
Tarun Panthi <tarunplaystation4@gmail.com>
2026-07-31 17:40:21 +0545

    Time: 0 ms (100%), Space: 16.6 MB (8.05%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int f(int row, int col, vector<vector<int>>& matrix,
+          vector<vector<int>>& dp, vector<vector<bool>>& vis) {
+
+        int n = matrix.size();
+
+        if (col < 0 || col >= n)
+            return INT_MAX;
+
+        if (row == 0)
+            return matrix[0][col];
+
+        if (dp[row][col]!=INT_MIN)
+            return dp[row][col];
+
+       
+
+        int left = f(row - 1, col - 1, matrix, dp, vis);
+        int up = f(row - 1, col, matrix, dp, vis);
+        int right = f(row - 1, col + 1, matrix, dp, vis);
+
+        int best = min({left, up, right});
+
+        if (best == INT_MAX)
+            return dp[row][col] = INT_MAX;
+
+        return dp[row][col] = matrix[row][col] + best;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        vector<vector<int>> dp(n, vector<int>(n,INT_MIN));
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
+
+        int ans = INT_MAX;
+
+        for (int j = 0; j < n; j++) {
+            ans = min(ans, f(n - 1, j, matrix, dp, vis));
+        }
+
+        return ans;
+    }
+};
