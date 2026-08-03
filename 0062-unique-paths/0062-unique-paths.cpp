b555fc4cf4318c2be5af66edad05d07b099f9f3b
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-03 14:08:06 +0530

    Time: 1 ms (26.88%), Space: 9.4 MB (54.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
+    int f(int m,int n,int i,int j , vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1)return 1;
+        if(i>m-1 || j>n-1)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int left=f(m,n,i+1,j,dp);
+        int right=f(m,n,i,j+1,dp);
+        return dp[i][j]=left+right;
+    }
     int uniquePaths(int m, int n) {
-        // by tabulation method simple iterative
-        vector<vector<int>>dp(m,vector<int>(n));
-        for(int j=0;j<n;j++){
-            dp[0][j]=1;
-        }
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1]; // bss yahi ek formula pata karna tha thats it. aur yeh easily diagram banakar aa gaya
-            }
-        }
-        return dp[m-1][n-1];
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return f(m,n,0,0,dp);
     }
 };
