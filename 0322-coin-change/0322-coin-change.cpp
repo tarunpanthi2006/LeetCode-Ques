1eedae0d818df5896494059d9883125217cb92d5
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-05 22:11:23 +0530

    Time: 50 ms (26.49%), Space: 53.5 MB (8.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int helper(vector<int>&coins,int amount,vector<int>&dp){
-        if(amount==0)return 0;
-        if (amount < 0) return INT_MAX;
-        if(dp[amount]!=-1)return dp[amount];
-        int mincoin=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int result = helper(coins,amount-coins[i],dp);
-            if(result!=INT_MAX){
-                mincoin=min(mincoin,result+1);
-            }
+    int f(int i,vector<int>& coins, int target,vector<vector<int>>&dp){
+        if(i==0){
+            if(target%coins[0]==0)return target/coins[0];
+            else return 1e9;
         }
-        dp[amount]=mincoin;
-        return dp[amount];
+        if(dp[i][target]!=-1)return dp[i][target];
+        // ya coin lunga ya nahi lunga
+
+        int nahilunga=0+f(i-1,coins,target,dp);
+        int lunga=INT_MAX;
+        if(coins[i]<=target)lunga = 1+f(i,coins,target-coins[i],dp);
+        return dp[i][target]=min(lunga,nahilunga);
     }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans= helper(coins,amount,dp);
-        if(ans==INT_MAX)return -1;
+    int coinChange(vector<int>& coins, int target) {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        int ans=f(n-1,coins,target,dp);
+        if(ans>=1e9)return -1;
         return ans;
     }
 };
