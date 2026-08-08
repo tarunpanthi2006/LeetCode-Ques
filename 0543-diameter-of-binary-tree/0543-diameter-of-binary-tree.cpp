03135c91d7e5585d1016e2715a53c0f205fde73f
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-08 17:18:04 +0530

    Time: 282 ms (5.54%), Space: 23.7 MB (73.47%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int helper(TreeNode* root,int &maxi){
+    int maxi=0;
+    int height(TreeNode*root){
         if(root==NULL)return 0;
-        int leftheight=helper(root->left,maxi);
-        int rightheight=helper(root->right,maxi);
-        maxi = max(maxi, leftheight + rightheight);
-        return 1+max(leftheight,rightheight);
+        return 1+max(height(root->left),height(root->right));
+    }
+    void f(TreeNode* root){
+        if(root==NULL)return ;
+        int rightheight=height(root->right);
+        int leftheight=height(root->left);
+        maxi=max(maxi,rightheight+leftheight);
+        f(root->left);
+        f(root->right);
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        int maxi=0;
-        helper(root,maxi);
+        f(root);
         return maxi;
     }
-    // basically iss code mein ky ahi raha hai ... helepr function left aur right subtree mein height nikal raha hai and maxi ko update kar raha hai then at last mai maxi k return kar deta hu
 };
