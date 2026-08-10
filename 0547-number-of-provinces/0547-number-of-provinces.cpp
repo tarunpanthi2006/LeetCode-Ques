88ab39ac488fe5ecb9209687defd3e5f26bd8af2
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-11 00:43:30 +0530

    Time: 0 ms (100%), Space: 19.8 MB (47.29%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    void bfs(vector<int>adj[],vector<int>&vis,int start,int v){
+    void bfs(vector<vector<int>>& grid,int start,vector<int>&vis){
         queue<int>q;
         q.push(start);
         vis[start]=1;
+        int n=grid.size();
         while(q.size()>0){
             int node=q.front();
             q.pop();
-            for(auto ele: adj[node]){
-                if(vis[ele]==0){
-                    vis[ele]=1;
-                    q.push(ele);
+            for(int j=0;j<n;j++){
+                if(grid[node][j]==1 && vis[j]==0){
+                    vis[j]=1;
+                    q.push(j);
                 }
             }
         }
     }
     int findCircleNum(vector<vector<int>>& grid) {
-        // mereko matrix diya hai so firstly mai ek adjlist banaunga
-        int v=grid.size();
-        vector<int>adj[v];
-        for(int i=0;i<v;i++){
-            for(int j=0;j<v;j++){
-                if(i!=j && grid[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
-        // list bann gayi yeh tarika yaad rakhlo
-        vector<int>vis(v,0);
+        int n=grid.size();
+        vector<int>vis(n,0);
         int ans=0;
-        for(int i=0;i<v;i++){
+        for(int i=0;i<n;i++){
             if(vis[i]==0){
                 ans++;
-                bfs(adj,vis,i,v);
+                bfs(grid,i,vis);
             }
         }
         return ans;
