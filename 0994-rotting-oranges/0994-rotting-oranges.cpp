100b8c83096bf5e9579c16711c7bfb69660a9e22
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-12 02:09:14 +0530

    Time: 0 ms (100%), Space: 17.1 MB (39.53%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int count = 0;
+    void bfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int& ans, queue<pair<int, int>>& q) {
         int m = grid.size();
         int n = grid[0].size();
-        while (1) {
-            bool flag = false;
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (grid[i][j] == 2) {
-                        if (j + 1 <= n - 1 && grid[i][j + 1] == 1) {
-                            grid[i][j + 1] = 3;
-                            flag = true;
-                        }
-                        if (j != 0 && grid[i][j - 1] == 1) {
-                            grid[i][j - 1] = 3;
-                            flag = true;
-                        }
-                        if (i != 0 && grid[i - 1][j] == 1) {
-                            grid[i - 1][j] = 3;
-                            flag = true;
-                        }
-                        if (i + 1 <= m - 1 && grid[i + 1][j] == 1) {
-                            grid[i + 1][j] = 3;
-                            flag = true;
+        while (q.size() > 0) { // iss loop se sabhi rotten otranges ek saarth dekh lenge per minute
+            int size = q.size();
+            bool rotted = false;
+            while (size > 0) { // current minute mein saare oranges ko dekh lenge
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+                for (int delrow = -1; delrow <= 1; delrow++) {
+                    for (int delcol = -1; delcol <= 1; delcol++) {
+                        int nrow = delrow + r;
+                        int ncol = delcol + c;
+                        if (nrow == r || ncol == c) {
+                            if (nrow >= 0 && nrow < m && ncol >= 0 &&
+                                ncol < n && grid[nrow][ncol] == 1 &&
+                                vis[nrow][ncol] == 0) {
+                                grid[nrow][ncol] = 2;
+                                vis[nrow][ncol] = 1;
+                                q.push({nrow, ncol});
+                                rotted = true;
+                            }
                         }
                     }
                 }
+                size--;
             }
-            if (!flag) break;
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (grid[i][j] == 3) {
-                        grid[i][j] = 2;
-                    }
-                }
-            }
-            count++;
+            if (rotted)
+                ans++;
         }
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        queue<pair<int, int>> q;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) return -1; // Impossible to rot all
+                if (grid[i][j] == 2 && vis[i][j] == 0) {
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                }
             }
         }
-        return count;
+        bfs(grid, vis, ans, q);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1)
+                    return -1;
+            }
+        }
+        return ans;
     }
 };
