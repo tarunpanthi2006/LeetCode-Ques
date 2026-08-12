b73e38b0f8431ad65d1d68a76a8cddcd670ed584
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-12 18:53:49 +0530

    Time: 26 ms (12.16%), Space: 21.9 MB (5.6%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int temp=100000007;
-        bool found =true;
-        while(found!=false){
-            found=false;
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(matrix[i][j]==temp){
-                        found=true;
-                        break;
-                    }
-                }
-                if(found==true)temp++;
-                else break;
-            }
-        }
+    void setZeroes(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int>rowvec;
+        vector<int>colvec;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    for(int x=0;x<n;x++){
-                        if(matrix[i][x]!=0){
-                            matrix[i][x]=temp;
-                        }
-                    }
-                    for(int x=0;x<m;x++){
-                        if(matrix[x][j]!=0){
-                            matrix[x][j]=temp;
-                        }
-                    }
-                    matrix[i][j]=temp;
+                if(grid[i][j]==0){
+                    rowvec.push_back(i);
+                    colvec.push_back(j);
                 }
             }
         }
-        for(int i=0;i<m;i++){
+        for(int x=0;x<rowvec.size();x++){
+            int i=rowvec[x];
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==temp)matrix[i][j]=0;
+                grid[i][j]=0;
+            }
+        }
+        for(int x=0;x<colvec.size();x++){
+            int j=colvec[x];
+            for(int i =0;i<m;i++){
+                grid[i][j]=0;
             }
         }
     }
