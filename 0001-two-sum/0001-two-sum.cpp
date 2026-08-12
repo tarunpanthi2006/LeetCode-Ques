5e9d95810c551a8b4154ab48e0e436e5addf0fca
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-12 15:20:38 +0530

    Time: 0 ms (100%), Space: 14.4 MB (63.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int>temp(n);
-        for(int i=0;i<n;i++){
-            temp[i]=nums[i];
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back(nums[i]);
         }
         vector<int>ans;
         sort(nums.begin(),nums.end());
         int i=0;
-        int j=n-1;
+        int j=nums.size()-1;
         int a=0;
         int b=0;
         while(i<j){
-            int sum=nums[i]+nums[j];
-            if(sum==target){
+            if(target==nums[i]+nums[j]){
                 a=nums[i];
                 b=nums[j];
                 break;
             }
-            else if(sum>target){
-                j--;
-            }
-            else{
+            else if(target>nums[i]+nums[j]){
                 i++;
             }
+            else j--;
         }
-        for(int i=0;i<n;i++){
-            if(temp[i]==a)ans.push_back(i);
-            else if(temp[i]==b)ans.push_back(i);
+        for(int i=0;i<v.size();i++){
+            if(v[i]==a)ans.push_back(i);
+            else if(v[i]==b)ans.push_back(i);
             if(ans.size()==2)break;
         }
         return ans;
