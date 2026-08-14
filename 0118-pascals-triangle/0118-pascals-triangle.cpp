43c438224da7ea193bea47eee8680faf35bb421e
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-14 21:54:44 +0530

    Time: 0 ms (100%), Space: 9.5 MB (99.35%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -7,7 +7,7 @@ public:
             ans.push_back(v);
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
+            for(int j=0;j<ans[i].size();j++){
                 if(j==0 || j==i)ans[i][j]=1;
                 else{
                     ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
