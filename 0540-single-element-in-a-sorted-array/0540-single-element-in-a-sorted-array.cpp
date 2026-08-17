fcb3584c24a40753fdb31881ac8cc910f884e2cd
Tarun Panthi <tarunplaystation4@gmail.com>
2026-08-18 00:35:52 +0530

    Time: 0 ms (100%), Space: 26.2 MB (69.81%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,8 +3,20 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
-        for(int i=0;i<n;i=i+2){
-            if(nums[i]!=nums[i+1])return nums[i];
+        if(nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        int lo=1; // coz zero ka case toh check kar liya 
+        int hi=n-2; // coz last wale ka case check ho gaya
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])return nums[mid];
+            // basicaly jab single element se pehele hu toh indexes(even,odd) hai and single element k baad indexes (odd,even).
+            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2!=0 && nums[mid]==nums[mid-1])){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
         }
         return -1;
     }
